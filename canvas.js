cd4e64f7b13d5038430aab6e355b2c4c15522470
Patrik Oxenstrand <Patrik.Oxenstrand@skola.taby.se>
2026-04-13 11:21:08 +0200

    Fixat så att vår test kub sitter fast och är inte relativ till fönstret

--- canvas.js
+++ canvas.js
@@ -1,16 +1,19 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 800;
+canvas.height = 600;
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Example: Draw a red rectangle
-    ctx.fillStyle = 'blu';
-    ctx.fillRect(625, 464, 100, 100);
+    ctx.fillStyle = 'black';
+    const x = canvas.width * 0.5;
+    const y = canvas.height * 0.5;
+    ctx.fillRect(x, y, 100, 100);
 }
-// Call the draw function to render the initial state
+
 draw();
 
-// Example: Update the canvas every second
 setInterval(() => {
     draw();
 }, 1000);  
