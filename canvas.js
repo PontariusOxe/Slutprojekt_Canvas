b688abd504caa977fecee1eb24ae7f74f6681b1d
Patrik Oxenstrand <Patrik.Oxenstrand@skola.taby.se>
2026-04-13 11:10:57 +0200

    lagt till bakgrund, massa sprites för fiende och har skapat html och css filer
    Co-authored-by: vidarberg-sys <vidarberg-sys@users.noreply.github.com>
    Co-authored-by: Fabinladin <fabiansamuelsson-ux@users.noreply.github.com>

--- canvas.js
+++ canvas.js
@@ -1,1 +1,16 @@
-Banan
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Example: Draw a red rectangle
+    ctx.fillStyle = 'blu';
+    ctx.fillRect(625, 464, 100, 100);
+}
+// Call the draw function to render the initial state
+draw();
+
+// Example: Update the canvas every second
+setInterval(() => {
+    draw();
+}, 1000);  
